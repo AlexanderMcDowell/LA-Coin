85975ff001eefef5427eecb664f49fb6c30e9575
Ben Falkenburg <46232936+Peter-Pseudonym@users.noreply.github.com>
2019-10-03 16:16:26 -0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,14 +2,14 @@
   <div class="ion-page">
 		<ion-content class="ion-padding">
 			<div class="ion-text-xl">
-				<h1 class="ion-margin-left">Wuffee</h1>
+				<h1 class="ion-margin-left">Home</h1>
 				
 			</div>
 		</ion-content>
 
 		<ion-toolbar position="bottom" class="ion-padding">
-			<ion-button expand="block" fill="outline">Get Started</ion-button>
-			<ion-button expand="block" class="ion-margin-top">Login</ion-button>
+			<ion-button expand="block" fill="outline" onclick="location.href='/#/signup'">Get Started</ion-button>
+			<ion-button expand="block" class="ion-margin-top" onclick="location.href='/#/login'">Login</ion-button>
 		</ion-toolbar>
   </div>
 </template>
