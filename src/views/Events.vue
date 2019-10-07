f0319054e0a46f49e5e94727731d7397db3ac155
Ben Falkenburg <46232936+Peter-Pseudonym@users.noreply.github.com>
2019-10-06 21:29:24 -0700

    Add files via upload

---
+++ src/views/Events.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="ion-page">
+      <ion-header>
+      <ion-toolbar>
+        <Navbar />
+        <ion-title>Events</ion-title>
+      </ion-toolbar>
+    </ion-header>
+      <ion-content class="ion-padding">
+          <p>Hi</p>
+      </ion-content>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import firebase from '@/firebase.config'
+import Navbar from "@/components/Navbar.vue";
+
+@Component({
+  components: {
+    Navbar
+  }
+})
+export default class Events extends Vue {
+
+}
+</script>
+
+<style scoped>
+</style>
