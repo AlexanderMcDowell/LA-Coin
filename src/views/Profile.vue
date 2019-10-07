3d9ec8dd7898344cb33b394c225129a62c63295f
Ben Falkenburg <46232936+Peter-Pseudonym@users.noreply.github.com>
2019-10-06 21:33:31 -0700

    Add files via upload

---
+++ src/views/Profile.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="ion-page">
+        <ion-header>
+            <ion-toolbar>
+                <Navbar />
+                <ion-title>Profile</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content class="ion-padding">
+            <pre>This page should do the following: 
+                clicking on a profile card on the 
+                people page should lead here, and a 
+                profile for the user should be generated. 
+                This can be a parameter or something.</pre>
+        </ion-content>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import firebase from '@/firebase.config'
+import Navbar from "@/components/Navbar.vue";
+
+
+@Component({
+  components: {
+    Navbar
+  }
+})
+export default class Profile extends Vue {
+}
+</script>
+
+<style scoped>
+</style>
