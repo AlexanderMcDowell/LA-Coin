225b86bcb38c7677ce478af027d4dd087df26b83
_nahoy <yohan.guyomard@gmail.com>
2019-10-09 22:25:13 -0700

    progress on profile page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -6,12 +6,22 @@
                 <ion-title>Profile</ion-title>
             </ion-toolbar>
         </ion-header>
-        <ion-content class="ion-padding">
-            <pre>This page should do the following: 
-                clicking on a profile card on the 
-                people page should lead here, and a 
-                profile for the user should be generated. 
-                This can be a parameter or something.</pre>
+
+        <ion-content>
+            <!-- Profile Icon & Name -->
+            <div id="profile">
+                <!-- Change this image with the actual user icon -->
+                <img id="profile-icon" src="https://www.chccw.org/wp-content/uploads/profile-icon-IMG-2-300x300.png">
+                <h1 id="profile-username"><span class="rank"><strong>RANK</strong></span> noobmaster69</h1>
+            </div>
+
+            <!-- Profile Overview -->
+            <div class="info-block">
+                <h1 id="balance">💰420,000</h1>
+                <span class="vertical-line"></span>
+                User Since: 00/00/00 <br>
+                Bio: This is your biography
+            </div>
         </ion-content>
     </div>
 </template>
@@ -21,7 +31,6 @@ import Vue from 'vue'
 import Component from 'vue-class-component'
 import firebase from '@/firebase.config'
 import Navbar from "@/components/Navbar.vue";
-
 
 @Component({
   components: {
@@ -33,4 +42,78 @@ export default class Profile extends Vue {
 </script>
 
 <style scoped>
+    @import url('https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap');
+
+    #profile-icon {
+        width: 7.5em;
+        height: 7.5em;
+        background-color: aquamarine;
+        border-radius: 45%;
+        display: inherit;
+        margin-left: inherit;
+        margin-right: inherit;
+        box-shadow: 0px 0.25em 0.25em #16161611;
+    }
+
+    #profile-username {
+        display: inherit;
+        margin-left: inherit;
+        margin-right: inherit;
+        text-align: center;
+        vertical-align: top;
+        font-family: 'Roboto Slab', serif;
+
+    }
+
+    #profile {
+        padding-top: 2em;
+        top: auto;
+        bottom: auto;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    .info-block {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 95%;
+        overflow: hidden;
+        background: rgb(67,76,170);
+        background: linear-gradient(347deg, rgba(67,76,170,1) 0%, rgba(93,110,201,1) 9%, rgba(0,212,255,1) 92%);
+        border-radius: 1em;
+        box-shadow: 0px 1em 2em #16161633;
+    }
+
+    #balance {
+        padding-left: 0.25em;
+        padding-top: 0.5em;
+        padding-bottom: 0.5em;
+        border: center;
+        float: left;
+        font-size: 3em;
+        font-family: 'Roboto Slab', serif;
+        color: rgb(243, 229, 229);
+        text-shadow: 0px 0.01em 0.2em rgba(29, 28, 28, 0.103);
+    }
+
+    .rank {
+        text-align: inherit;
+        background-color: blueviolet;
+        border-radius: 0.5em;
+        color: white;
+        border-width: 10em;
+        font-size: 0.5em;
+        padding: 0.25em;
+        height: 100%;
+    }
+
+    .vertical-line {
+        background-color: rgba(151, 146, 146, 0.267);
+        width: 0.1em;
+        height: 8.5em;
+        margin-left: 3em;
+        float: left;
+        display: block;
+    }
 </style>
