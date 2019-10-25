956a629ccad83bfca674f91df48363b862da55ec
Ben Falkenburg <46232936+Peter-Pseudonym@users.noreply.github.com>
2019-10-24 20:34:58 -0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,9 +7,10 @@
       </ion-toolbar>
     </ion-header>
 		<ion-content class="ion-padding">
-			<h1>LACoin</h1>
-			<div>
-				<img src="https://firebasestorage.googleapis.com/v0/b/wuffee-app.appspot.com/o/eagle.jpg?alt=media&token=da6e77ad-61d5-47b3-9ab3-a4fe860402a5"/>
+			<div class="logo-set">
+				<h1>LaCoin</h1>
+				<div class="logo-img"><img src="https://firebasestorage.googleapis.com/v0/b/wuffee-app.appspot.com/o/eagle.jpg?alt=media&token=da6e77ad-61d5-47b3-9ab3-a4fe860402a5"/></div>
+				<div class="info-p"><h2>Greed is Good. <br> Get Started Below</h2></div>
 			</div>
 		</ion-content>
 		<ion-footer class="ion-padding">
@@ -37,45 +38,38 @@ export default class Home extends Vue{
 </script>
 
 <style scoped>
-h1 {
-	font-style: italic;
-	font-size: 48px;
-	font-weight: bold;
-	text-align: center;
-}
+
 ion-footer {
 	--padding-top: 0;
 	--padding-bottom: 0;
 }
-@media screen and (orientation:portrait) {
-	img {
-		border: 2px solid;
-		border-radius: 50%;
-		border-color: navy;
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		height: 50%;
-		width: auto;
-		/* bring your own prefixes */
-		transform: translate(-50%, -50%);
-	}
+.logo-set {
+	margin:0;
 }
- @media screen and (orientation:landscape) { 
-	 img {
-		border: 2px solid;
-		border-radius: 50%;
-		border-color: navy;
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		height: 50%;
-		width: auto;
-		/* bring your own prefixes */
-		transform: translate(-50%, -50%);
-	}
-	ion-button {
-		display: inline-block;
-	}
+.logo-img img {
+	margin-top: 20vh;
+	border: 2px solid;
+	border-radius: 50%;
+	border-color: navy;
+	position: absolute;
+	left: 50%;
+	height: 50%;
+	width: auto;
+	/* bring your own prefixes */
+	transform: translate(-50%, -50%);
 }
+h1 {
+	margin: 0;
+	padding: 0;
+	font-style: italic;
+	font-size: 6vh;
+	font-weight: bold;
+	text-align: center;
+	}
+.info-p h2 {
+		padding-top: 90%;
+		color: gray;
+		text-align: center;
+		font-size: 3vh;
+	}
 </style>
