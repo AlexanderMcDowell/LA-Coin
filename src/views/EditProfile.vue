75e988a980cd017f3d4dcf97836e7f2a19a6df0e
Ben Falkenburg <46232936+Peter-Pseudonym@users.noreply.github.com>
2019-10-24 20:38:16 -0700

    Add files via upload

---
+++ src/views/EditProfile.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="ion-page">
+        <ion-header>
+            <ion-toolbar>
+                <Navbar />
+                <ion-title>Settings</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <h2> hi </h2>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import firebase from '@/firebase.config'
+import Navbar from "@/components/Navbar.vue";
+@Component({
+  components: {
+    Navbar
+  }
+})
+export default class EditProfile extends Vue {
+  /*const ref = firebase.storage().ref();
+  const file = document.querySelector('#photo').files[0]
+  const name = (+new Date()) + '-' + file.name;
+  const metadata = {
+    contentType: file.type
+  };
+  const task = ref.child(name).put(file, metadata);
+  task
+    .then(snapshot => snapshot.ref.getDownloadURL())
+    .then((url) => {
+      console.log(url);
+      document.querySelector('#someImageTagID').src = url;
+    })
+    .catch(console.error);*/
+}
+</script>
+
+<style scoped>
+    
+</style>
