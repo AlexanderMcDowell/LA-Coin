9bb81c80b9e9649c2e589ace75e04ce70703b604
Ben Falkenburg <46232936+Peter-Pseudonym@users.noreply.github.com>
2019-10-24 20:33:56 -0700

    Update Account.vue

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -7,23 +7,20 @@
       </ion-toolbar>
     </ion-header>
     <ion-content class="ion-padding">
-      <ion-card>
+      <ion-card class="profile-card">
         <ion-card-header>
-          <ion-card-title>
-            Welcome back,
-            <br />
-            {{ name }}.
-          </ion-card-title>
+          <router-link :to="{name: 'profile', params: { UserID: {balance: balance, name: name, bio: bio, sign_on_date: sign_on_date}}}">
+          <ion-card-title>Welcome Back, {{ name }}!</ion-card-title>
+          </router-link>
         </ion-card-header>
+        <ion-card-content>
+          <img id="profile-icon" src="https://www.chccw.org/wp-content/uploads/profile-icon-IMG-2-300x300.png">
+          <p id="bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+        </ion-card-content>
       </ion-card>
-      <ion-card>
-        <ion-card-header>
-          <ion-card-subtitle>Your Balance</ion-card-subtitle>
-          <ion-card-title>${{ balance }}</ion-card-title>
-        </ion-card-header>
-      </ion-card>
-      <Transactions />
+      
       <Graph />
+      <Transactions />
       <QRModal />
     </ion-content>
   </div>
@@ -50,22 +47,79 @@ import Navbar from "@/components/Navbar.vue";
 export default class Account extends Vue {
   name: string = "";
   balance: string = "";
+  bio: string = "";
+  sign_on_date: string = "";
+  //transactions: Object[] = [];
 
-  getBalance() {
+  getInfo() {
     var userId = firebase.auth.currentUser.uid;
     var user = firebase.usersCollection.doc(userId);
 
     user.get().then(doc => {
+      this.name = doc.data().name;
       this.balance = doc.data().balance;
+      this.bio = doc.data().bio;
+      this.sign_on_date = doc.data().sign_on_date;
     });
   }
 
   created() {
+    const currentUser = firebase.auth.currentUser.uid;
+    /*const transactionsDoc = firebase.usersCollection.doc(currentUser).collection("transactions");
+    transactionsDoc.get().then(snapshot => {
+        snapshot.forEach(doc => {
+            this.transactions.push(doc.data());
+                });
+            });
+    this.transactions = this.transactions[0]*/
+    /*<h2 id="update-balance" v-if="transactions.amount > 0" color="success">Your budget: {{ balance }} LaCoin</h2>
+      <h2 id="update-balance" v-if="transactions.amount > 0" color="success">Your budget: {{ balance }} LaCoin</h2>*/
     this.name = firebase.auth.currentUser.displayName;
-    this.getBalance();
+    this.getInfo();
   }
 }
 </script>
 
 <style scoped>
+ion-card {
+    padding-left: 0;
+    margin-left:10%;
+    --div-width: 80%;
+	  width: var(--div-width);
+	  height: calc(var(--div-width) * 0.9 / 3);
+}
+.profile-card {
+    background-image: linear-gradient(to bottom right, navy, blue);
+  }
+ion-card-content{
+    margin-left:-2%;
+    display:flex;
+  }
+ion-card-title {
+  text-transform: uppercase;
+  color: white;
+  margin-bottom: 1vh;
+}
+#profile-icon {
+    margin-top: -3vh;
+    width: calc(var(--div-width) * 1 / 3);
+    height: calc(var(--div-width) * 1 / 3);
+    border-radius:50%;
+  }
+#bio {
+  margin-top: -3vh;
+  color: white;
+  font-size: 1.5vh;
+  text-align: justify;
+  padding-left: 5%;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+.profile-card a {
+  text-decoration: none;
+}
+#update-balance {
+  text-align: center;
+  font-weight: bold;
+}
 </style>
