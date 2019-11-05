1b5455d098ee79d46229ddb8384c34f1055f42ea
Ben Falkenburg <46232936+Peter-Pseudonym@users.noreply.github.com>
2019-11-04 22:42:51 -0800

    Update Account.vue
    Pretty much finished profile

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -1,55 +1,76 @@
+<!--script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script-->
 <template>
   <div class="ion-page">
     <ion-header>
       <ion-toolbar>
-        <Navbar />
         <ion-title>Account</ion-title>
+        <Navbar />
       </ion-toolbar>
     </ion-header>
     <ion-content class="ion-padding">
+      <h1>Welcome Back!</h1>
       <ion-card class="profile-card">
         <ion-card-header>
           <router-link :to="{name: 'profile', params: { UserID: {balance: balance, name: name, bio: bio, sign_on_date: sign_on_date}}}">
-          <ion-card-title>Welcome Back, {{ name }}!</ion-card-title>
+          <ion-card-title v-if="name.length < 10">Hello, {{ name }}</ion-card-title>
+          <ion-card-title v-else>Hi {{ name }}</ion-card-title>
           </router-link>
         </ion-card-header>
         <ion-card-content>
-          <img id="profile-icon" src="https://www.chccw.org/wp-content/uploads/profile-icon-IMG-2-300x300.png">
-          <p id="bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+          <div id="icons">
+            <p>⬜️ ⬜️</p>
+          </div>
+          <div class="top-info">
+            <div class="icon-div">
+              <img v-bind:src="profile_photo">
+              <p>User Since {{ sign_on_date }}!</p>
+            </div>
+            <h2 id="balance"> 💰 {{balance}}.00</h2>
+          </div>
         </ion-card-content>
       </ion-card>
-      
       <Graph />
       <Transactions />
-      <QRModal />
     </ion-content>
   </div>
 </template>
+<script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
 
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
 import firebase from "@/firebase.config";
 
-import QRModal from "@/components/QRModal/QRModal.vue";
 import Transactions from "@/components/Transactions.vue";
 import Graph from "@/components/Graph.vue";
 import Navbar from "@/components/Navbar.vue";
 
 @Component({
   components: {
-    QRModal,
     Transactions,
-    Graph,
-    Navbar
+    Navbar,
+    Graph
   }
 })
 export default class Account extends Vue {
   name: string = "";
-  balance: string = "";
-  bio: string = "";
+  balance: number
   sign_on_date: string = "";
+  profile_photo: string = "";
   //transactions: Object[] = [];
+
+  transferCoin() {
+    var transaction = firebase.transfer({
+      fromId: "bcqYpPVZWphSVyNK4c4g",
+      toId: "pLt0jpah27BdIpjsoxRO",
+      amount: 50,
+      description: "Thanks for the merch"
+    });
+
+    transaction.then(res => {
+      console.log(res)
+    })
+  }
 
   getInfo() {
     var userId = firebase.auth.currentUser.uid;
@@ -58,62 +79,83 @@ export default class Account extends Vue {
     user.get().then(doc => {
       this.name = doc.data().name;
       this.balance = doc.data().balance;
-      this.bio = doc.data().bio;
       this.sign_on_date = doc.data().sign_on_date;
+      this.profile_photo = doc.data().profile_photo;
     });
+    /*const transactionsDoc = firebase.usersCollection.doc(userId).collection("transactions");
+            transactionsDoc.get().then(snapshot => {
+                snapshot.forEach(doc => {
+                    this.transactions.push(doc.data());
+                });
+            });*/
   }
 
   created() {
     const currentUser = firebase.auth.currentUser.uid;
-    /*const transactionsDoc = firebase.usersCollection.doc(currentUser).collection("transactions");
-    transactionsDoc.get().then(snapshot => {
-        snapshot.forEach(doc => {
-            this.transactions.push(doc.data());
-                });
-            });
-    this.transactions = this.transactions[0]*/
-    /*<h2 id="update-balance" v-if="transactions.amount > 0" color="success">Your budget: {{ balance }} LaCoin</h2>
-      <h2 id="update-balance" v-if="transactions.amount > 0" color="success">Your budget: {{ balance }} LaCoin</h2>*/
     this.name = firebase.auth.currentUser.displayName;
     this.getInfo();
+    //this.transferCoin();
   }
 }
 </script>
 
 <style scoped>
-ion-card {
-    padding-left: 0;
-    margin-left:10%;
-    --div-width: 80%;
-	  width: var(--div-width);
-	  height: calc(var(--div-width) * 0.9 / 3);
+@import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+ion-content{
+  font-family: 'Roboto', serif;
+}
+body {
+  margin: 0;
+  padding: 0;
 }
 .profile-card {
-    background-image: linear-gradient(to bottom right, navy, blue);
-  }
-ion-card-content{
-    margin-left:-2%;
-    display:flex;
-  }
+  /*background-image: linear-gradient(to bottom right, skyblue, aquamarine); //In the Green */
+  background-image: linear-gradient(to bottom right, skyblue, violet);
+  /*background-image: linear-gradient(to bottom right, orange, crimson); //In the Red*/
+}
+ion-card {
+  width: 95%;
+	height: 55vw;
+  border-radius: 3vw;
+}
 ion-card-title {
   text-transform: uppercase;
-  color: white;
-  margin-bottom: 1vh;
+  padding-bottom: 2.5vw;
+  font-size: 6vw;
 }
-#profile-icon {
-    margin-top: -3vh;
-    width: calc(var(--div-width) * 1 / 3);
-    height: calc(var(--div-width) * 1 / 3);
-    border-radius:50%;
-  }
-#bio {
-  margin-top: -3vh;
-  color: white;
-  font-size: 1.5vh;
-  text-align: justify;
-  padding-left: 5%;
+#icons {
+  float: right;
+  margin-top: -16.25vw;
+}
+#icons p {
+  font-size: 7.5vw;
+}
+.top-info {
+  display: flex;
+}
+.icon-div {
+margin-top: -5vw; 
+}
+.icon-div img {
+  width: 33vw;
+  height: auto;
+  border: 2px solid;
+  border-radius: 50%;
+  border-color: lightgray;
+}
+.icon-div p {
+  width: 300%;
+  font-size: 4vw;
+}
+#balance {
+  margin-top: -2.5vw;
+  margin-left: -7.5vw;
+  font-size: 9vw;
   overflow: hidden;
-  text-overflow: ellipsis;
+}
+#balance, .icon-div p, ion-card-title {
+  font-weight: bold;
+  color: white;
 }
 .profile-card a {
   text-decoration: none;
@@ -121,5 +163,9 @@ ion-card-title {
 #update-balance {
   text-align: center;
   font-weight: bold;
+}
+ion-icon {
+  border:2px solid;
+  border-color: red;
 }
 </style>
