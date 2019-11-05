d428f8cd5bd43bd37bed1f678fafeb117e437edd
Ben Falkenburg <46232936+Peter-Pseudonym@users.noreply.github.com>
2019-11-04 22:42:13 -0800

    Update Transactions.vue
    This returns a transactions card

--- src/components/Transactions.vue
+++ src/components/Transactions.vue
@@ -1,22 +1,20 @@
 <template>
-    <ion-list>
-        <ion-card>
-            <ion-card-header>
-                <ion-card-title>Your LaCoin Transactions!</ion-card-title>
-            </ion-card-header>
-        </ion-card>
-        <ion-card v-for="transaction in transactions">
-            <ion-card-content>
+    <ion-card>
+        <ion-card-header>
+            <ion-card-title>Your LaCoin Transactions!</ion-card-title>
+        </ion-card-header>
+        <ion-card-content>
+            <ion-item v-for="transaction in transactions">
                 <ion-label v-if="transaction.amount > 0" color="success">
                     +{{ transaction.amount }}
                 </ion-label>
                 <ion-label v-if="transaction.amount < 0" color="danger">
-                    -{{ transaction.amount }}
+                    {{ transaction.amount }}
                 </ion-label>
                     {{ transaction.description }}
-            </ion-card-content>
-        </ion-card>
-    </ion-list>
+            </ion-item>
+        </ion-card-content>
+    </ion-card>
 </template>
 
 <script lang="ts">
@@ -25,16 +23,23 @@
     import firebase from "@/firebase.config";
     @Component
     export default class Transactions extends Vue {
-        
-        transactions: Object[] = []
+        transactions: object[] = []
         created() {
-            const currentUser = firebase.auth.currentUser.uid;
+            var userId = firebase.auth.currentUser.uid;
+            var user = firebase.usersCollection.doc(userId);
+            user.get().then(doc => {
+                this.transactions = doc.data().transactions;
+            });
+            /*const currentUser = firebase.auth.currentUser.uid;
             const transactionsDoc = firebase.usersCollection.doc(currentUser).collection("transactions");
             transactionsDoc.get().then(snapshot => {
                 snapshot.forEach(doc => {
                     this.transactions.push(doc.data());
+                    console.log('transaction')
+                    console.log(doc.data())
+                    console.log('transaction')
                 });
-            });
+            });*/
         }
     }
 </script>
@@ -42,8 +47,8 @@
 <style scoped>
 ion-card {
     padding-left: 0;
-    margin-left:10%;
-    width: 80%;
+    margin-left:2.5%;
+    width: 95%;
     height:auto;
 }
 </style>
