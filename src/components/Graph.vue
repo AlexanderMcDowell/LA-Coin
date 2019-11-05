30fe1c6465168005bc0a5655d39f62e2b7a3b1d8
Ben Falkenburg <46232936+Peter-Pseudonym@users.noreply.github.com>
2019-11-04 22:38:34 -0800

    Update Graph.vue
    This page just goes through the transactions (I set up one the Sign Up Page), and does a standard summing so you can see your balance. This page may need to be updated if transactions are modified.

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -1,27 +1,50 @@
 <template>
-    <ion-card>
+    <ion-card id="graph-container">
         <ion-card-header>
-            <ion-card-title>Your LaCoin!</ion-card-title>
+            <ion-card-title>Your LACoin Over Time</ion-card-title>
         </ion-card-header>
             <ion-card-content>
-                <line-chart :data="{'2017-05-13': 2, '2017-05-14': 5}"></line-chart>
+                <area-chart :data="chartData" height="75vw"></area-chart>
             </ion-card-content>
     </ion-card>
 </template>
+
 <script lang="ts">
-    import Vue from "vue";
+    import Vue from 'vue';
+    import Chartkick from 'vue-chartkick';
+    import Chart from 'chart.js';
     import Component from "vue-class-component";
     import firebase from "@/firebase.config";
-
-    import Chartkick from 'vue-chartkick'
-    import Chart from 'chart.js'
-
     Vue.use(Chartkick.use(Chart))
 
     @Component
     export default class Graph extends Vue {
-        transactionsNum: number[] = []
-        transactionsDate: string[] = []
+        chartData: object[] = []
+        balance: any
+
+        created() {
+            var userId = firebase.auth.currentUser.uid;
+            var user = firebase.usersCollection.doc(userId);
+            user.get().then(doc => {
+                //console.log(doc.data().transactions[0])
+                this.balance = doc.data().balance;
+                var compiledBalance = this.balance;
+                for (var i = 0; i < doc.data().transactions.length; i++) {
+                    var transaction = doc.data().transactions[i];
+                    var date = transaction.date
+                    var amount = transaction.amount
+                    compiledBalance = compiledBalance + transaction.amount;
+                    //console.log(transaction)
+                    this.chartData.push([date, compiledBalance])
+                }
+            });
+            console.log(this.chartData)
+            /*this.chartData = {'1':'1', '2':'2'}
+            console.log('shift')
+            console.log(this.chartData)*/
+        }
+    }
+        /* transactionsNum: number[] = []
         created() {
             const currentUser = firebase.auth.currentUser.uid;
             const user = firebase.usersCollection.doc(currentUser);
@@ -33,7 +56,15 @@
                 });
             });
         }
-    }
+        */
 </script>
+
 <style scoped>
+#graph-container {
+    height: 100vw;
+}
+area-chart {
+    margin: 0;
+    padding: 0;
+}
 </style>
