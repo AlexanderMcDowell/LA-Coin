70c5f9fdc0f67b2d7415957dfe5ceccbed37e626
Ben Falkenburg <46232936+Peter-Pseudonym@users.noreply.github.com>
2019-11-28 17:12:14 -1000

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,4 +1,4 @@
-<!--script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script-->
+<script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
 <template>
     <ion-buttons slot="secondary">
         <ion-button color="primary" shape="round" fill="outline" strong="true" onclick="location.href='/#/account'">Account</ion-button>
@@ -19,11 +19,24 @@
 </script>
 
 <style scoped>
+ion-buttons {
+    padding-top: 2vw;
+    padding-bottom: 2vw;
+    width: 100vw;
+    display: flex;
+    justify-content: space-evenly;
+    margin-bottom: 0;
+    margin-left: 0;
+    margin-right: 0;
+    background-color: rgb(250, 250, 250);
+}
 ion-button {
-    width: 50px;
-    height: 50px;
-    font-size: 8px;
+    width: 75px;
+    height: 75px;
+    font-weight: bold;
+    font-size: 12.5px;
     border-radius: 50%;
     color: primary;
+    background-color: white;
 }
 </style>
