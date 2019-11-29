7a3ebe0ccbf5f4ce235f8fa60e089f4ee3163cc3
Ben Falkenburg <46232936+Peter-Pseudonym@users.noreply.github.com>
2019-11-28 17:09:39 -1000

    Update Signup.vue

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -2,11 +2,11 @@
 	<div class="ion-page">
 		<ion-header>
 			<ion-toolbar>
-                <ion-title>Sign Up</ion-title>
-                <Navbar />
-            </ion-toolbar>
+        		<ion-title>Sign Up</ion-title>
+      		</ion-toolbar>
 		</ion-header>
 		<ion-content class="ion-padding">
+			<!-- Sign up form-->
 			<form @submit="signup">
 				<ion-item>
 					<ion-label position="floating">Name</ion-label>
@@ -32,58 +32,92 @@
 </template>
 
 <script lang="ts">
+	//Imports
 	import Vue from 'vue'
 	import Component from 'vue-class-component'
 	import firebase from '@/firebase.config'
 	import Navbar from "@/components/Navbar.vue";
 	​
+	//Define as Vue component
 	@Component
+
 	export default class Signup extends Vue {
-	​
-		name: string = ""
-		email: string = ""
-		password: string = ""
-		balance: number = 250
-		bio: string = "Add desciption!"
-		profile_photo: string = ""
-		event_tickets: string[] = []
-				
+
+		// All User fields
+		name: string = "";
+		balance: number = 0;
+		bio: string = "Add desciption!";
+		email: string = "";
+		password: string = "";
+		profile_photo: string = "";
+		event_tickets: string[] = [];
+		friends: string[] = [];
+		todayDate: string = "";
+		startDate: string = "";
+		graphSpec: number = 10;
+
+		// Initialize User transactions, notifications
+		//transactions: object[] = [{date:this.todayDate, amount:250, description:"Welcome to LaCoin!", sender_id: 'admin'}];
+		//unreadNotif: object[] = [{date:this.todayDate, type:'welcome_message', sentfrom:'admin', description:"Welcome to LaCoin!"}];
+
+		// Get the date
+		created() {
+			this.getDate();
+		}
+
+		getDate() {
+			var today = new Date();
+			var dd = String(today.getDate()).padStart(2, '0');
+			var mm = String(today.getMonth() + 1).padStart(2, '0');
+			var yyyy = today.getFullYear();
+			this.todayDate = mm + '/' + dd + '/' + yyyy;
+		}
+
+		// User condition -- sign up
 		signup(e: Event) {
 			firebase.auth.createUserWithEmailAndPassword(this.email, this.password).then((user) => {
-				var today = new Date();
-				var dd = String(today.getDate()).padStart(2, '0');
-				var mm = String(today.getMonth() + 1).padStart(2, '0');
-				var yyyy = today.getFullYear();
-				var sign_on_date = mm + '/' + dd + '/' + yyyy;
+				
+				// Set up Firebase User calling
+				var newUser = firebase.usersCollection.doc(user.user.uid);
 
+				// Profile image array
 				var profileChoices = ["https://firebasestorage.googleapis.com/v0/b/wuffee-app.appspot.com/o/ProfileOne.jpg?alt=media&token=d0e7524d-9e7b-43e5-b4ca-e0150a8a0544",
 									"https://firebasestorage.googleapis.com/v0/b/wuffee-app.appspot.com/o/ProfileTwo.jpg?alt=media&token=f58c01a6-68dc-42ad-9a2f-f7da1aafa3a5",
 									"https://firebasestorage.googleapis.com/v0/b/wuffee-app.appspot.com/o/ProfileThree.jpg?alt=media&token=967d4034-126d-44d1-b604-29930fe14e6d",
 									"https://firebasestorage.googleapis.com/v0/b/wuffee-app.appspot.com/o/ProfileFour.jpg?alt=media&token=102b0ce2-69f0-4bd8-ac3f-ce15479277c1"
-				]
-				
-				var userDoc = firebase.usersCollection.doc(user.user.uid)
-				
+									];
+
+				// Set new user display name
 				user.user.updateProfile({
 					displayName: this.name
 				});
-	​
-				userDoc.set({
+
+				//console.log(this.unreadNotif)
+				//console.log(this.transactions)
+
+				// Set new user fields
+				newUser.set({
 					name: this.name,
-					balance: this.balance,
-					sign_on_date: sign_on_date,
+					sign_on_date: this.todayDate,
 					bio: this.bio,
 					profile_photo: profileChoices[Math.floor(Math.random()*profileChoices.length)],
 					event_tickets: this.event_tickets,
-					unreadNotif: {},
-					transactions: [{date:'2017-01-01 00:00:00 -0800', amount:2, description:"Thanks!!"}, {date:'2017-01-01 00:01:00 -0800', amount:-5, description:"Thanks!!"}]
+					unreadNotif: [{date:this.todayDate, type:'welcome_message', sentfrom:'admin', description:"Welcome to LaCoin!"}],
+					transactions: [{date:this.todayDate, amount:250, description:"Welcome to LaCoin!", sender_id: 'admin'}],
+					//balance: this.balance,
+					friends: this.friends,
+					graphSpec: this.graphSpec
 				})
-	​
-				this.$router.push('/account')
+
+				// Send data to new page
+				this.$router.push('/account');
 			})
+
+			// Cleanup
 			e.preventDefault();
 		}
-			
+		
+		// Finish account setup
 		googleSignup() {
 		}
 	}
@@ -91,6 +125,9 @@
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap');
+ion-title {
+  margin-left: 0;
+}
 ion-content {
   font-family: 'Roboto Slab', serif;
 }
