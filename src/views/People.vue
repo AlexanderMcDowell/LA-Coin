f9f89aabe73539ae8dab2ae38a4e093261ce4fb3
Ben Falkenburg <46232936+Peter-Pseudonym@users.noreply.github.com>
2019-11-28 17:08:57 -1000

    Update People.vue

--- src/views/People.vue
+++ src/views/People.vue
@@ -3,12 +3,48 @@
     <ion-header>
       <ion-toolbar>
         <ion-title>People</ion-title>
-        <Navbar />
       </ion-toolbar>
     </ion-header>
 		<ion-content class="ion-padding">
+
+      <!-- Instructions -->
+
+      <ion-card id="intro-card">
+          <ion-card-header>
+            <ion-card-title>Welcome to the LACoin Community!</ion-card-title>
+          </ion-card-header>
+          <ion-card-content id="content-text">
+            <img src="https://firebasestorage.googleapis.com/v0/b/wuffee-app.appspot.com/o/eagle.jpg?alt=media&token=da6e77ad-61d5-47b3-9ab3-a4fe860402a5"/>
+            <p>Click on a person / friend to reach out, make a transaction, and more!</p>
+          </ion-card-content>
+        </ion-card>
+      
+      <!-- List of your Friends-->
+      <div id="friends" v-if="friends.length > 0">
+        <h1>Your friends</h1>
+        <ion-card id="friend-info-card" v-for="friend in friends" v-bind:key="friend">
+          <ion-card-header>
+            <router-link :to="{name: 'profile', params: friend}">
+              <ion-card-title>{{ friend.data.name }}</ion-card-title>
+            </router-link>
+          </ion-card-header>
+          <ion-card-content>
+            <div class="basic-info">
+              <img class="profile-icon" v-bind:src="friend.data.profile_photo">
+              <p>{{ friend.data.balance }} LACoin </p>
+            </div>
+            <div class="card-description">
+              <p>{{ friend.data.bio }}</p>
+            </div>
+          </ion-card-content>
+        </ion-card>
+      </div>
+
       <ion-list>
-        <ion-card class="info-card" v-for="person in people" v-bind:key="person">
+        <!-- List of all users, no friends yet-->
+
+        <h1>All Users</h1>
+        <ion-card id="info-card" v-for="person in people" v-bind:key="person">
           <ion-card-header>
             <router-link :to="{name: 'profile', params: person}">
               <ion-card-title>{{ person.data.name }}</ion-card-title>
@@ -26,6 +62,11 @@
         </ion-card>
       </ion-list>
 		</ion-content>
+    <ion-footer>
+      <ion-toolbar>
+        <Navbar />
+      </ion-toolbar>
+    </ion-footer>
   </div>
 </template>
 
@@ -40,36 +81,77 @@ import Navbar from "@/components/Navbar.vue";
     Navbar
   }
 })
+
 export default class People extends Vue {
-    people: string[] = []
+    // Define people as object list
+    people: object[] = [];
+    ids_of_friends: string[] = [];
+    friends: object[] =[];
 
     created() {
-        this.getPeople()
+      this.getPeople();
     }
 
+    // Get data of all users
     getPeople() {
         var users = firebase.usersCollection
+        var userId = firebase.auth.currentUser.uid;
+        var user = firebase.usersCollection.doc(userId);
+        user.get().then(doc => {
+            this.ids_of_friends = doc.data().friends;
+        });
 
         users.get().then(snapshot => {
             snapshot.forEach(doc => {
-                this.people.push({id: doc.id, data: doc.data()})
+                var user_info = {id: doc.id, data: doc.data()};
+                user_info.data.balance = this.getBalance(user_info.data.transactions);
+                this.people.push(user_info);
+                if (this.ids_of_friends.includes(user_info.id)){
+                  this.friends.push(user_info)
+                }
             })
         })
+        console.log(this.people)
     }
+    getBalance(transactionDoc: object[]) {
+    var startBalance = 0;
+    //console.log(transactionDoc)
+		for (var i = 0; i < transactionDoc.length; i++) {
+      var transaction = transactionDoc[i];
+      //console.log(transaction)
+			startBalance = startBalance + transaction.amount;
+    }
+    return startBalance;
+  }
 }
 </script>
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap');
+ion-title {
+  margin-left: 0;
+}
 ion-content {
   font-family: 'Roboto Slab', serif;
 }
-.info-card {
+#info-card {
     display: inline-block;
     width: 75vw;
     height: 43.42vw;
-    background-image: linear-gradient(to bottom right, rgb(233, 249, 255), rgb(238, 236, 255));
-  }
+    background:
+      linear-gradient(217deg, rgba(241, 249, 255, 0.8), rgba(0, 81, 255, 0) 70.71%),
+      linear-gradient(127deg, rgba(196, 196, 196, 0.8), rgba(0, 255, 157, 0) 70.71%),
+      linear-gradient(336deg, rgba(182, 211, 255, 0.8), rgba(0,0,255,0) 70.71%);
+}
+#friend-info-card {
+    display: inline-block;
+    width: 75vw;
+    height: 43.42vw;
+    background:
+      linear-gradient(217deg, rgba(255, 219, 165, 0.8), rgba(0, 81, 255, 0) 70.71%),
+      linear-gradient(127deg, rgba(255, 223, 176, 0.8), rgba(0, 255, 157, 0) 70.71%),
+      linear-gradient(336deg, rgba(241, 215, 208, 0.8), rgba(0,0,255,0) 70.71%);
+}
 ion-card-content{
     margin-left:-2%;
     display:flex;
@@ -78,6 +160,12 @@ ion-card-header {
       text-transform: uppercase;
       color: black;
   }
+ion-footer {
+  background-color: rgb(250, 250, 250);
+}
+ion-toolbar {
+  background-color: rgb(250, 250, 250);
+}
 .profile-icon {
   width: 20vw;
   height: auto;
@@ -99,7 +187,22 @@ ion-card-header {
   padding-right: 1vw;
   overflow: auto;
 }
-.info-card a {
+#info-card a, #friend-info-card a {
   text-decoration: none;
+}
+#intro-card {
+  text-align: center;
+}
+#content-text {
+  display: inline-block;
+}
+#content-text img {
+  border: solid 2px;
+  border-color: navy;
+  border-radius: 50%;
+  margin-left: 25%;
+  margin-bottom: 5vw;
+  width: 50%;
+  height: auto;
 }
 </style>
